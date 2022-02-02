aa83fee6ad5445704e1b4163345c25e18c70a5ea
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-02-02 15:56:07 +1100

    vars

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+NAMESPACE=magento
+CRONJOB=ap3-stg-magento-setup-upgrade
+
 echo "--- Getting Magento pod name"
 POD=$(kubectl get pods -l autopilothq.com/name=magento --namespace magento -o jsonpath='{.items[*].metadata.name}' --field-selector status.phase=Running --sort-by=.metadata.creationTimestamp | head -n 1)
 echo "Pod name is $POD"
@@ -10,22 +13,22 @@ echo "--- Packaging"
 tar cfvz /tmp/magento-connector-php.tgz --exclude=.git --exclude=.gitignore --exclude=.buildkite .
 
 echo "--- Copying to $POD"
-kubectl cp -n magento /tmp/magento-connector-php.tgz "$POD":/bitnami/magento/
+kubectl cp -n $NAMESPACE /tmp/magento-connector-php.tgz "$POD":/bitnami/magento/
 
 echo "--- Trigger php bin/magento setup:upgrade"
 
 echo "Deleting existing job if it exists"
-kubectl -n magento delete job ap3-stg-magento-setup-upgrade-manual || :
+kubectl -n $NAMESPACE delete job ${CRONJOB}-manual || :
 
 echo "Triggering new job from cronjob"
-kubectl -n magento create job --from=cronjob/ap3-stg-magento-setup-upgrade ap3-stg-magento-setup-upgrade-manual
+kubectl -n $NAMESPACE create job --from=cronjob/${CRONJOB} ${CRONJOB}-manual
 
 echo "Waiting for the job to complete"
-kubectl -n magento wait --for=condition=complete job/ap3-stg-magento-setup-upgrade-manual &
+kubectl -n $NAMESPACE wait --for=condition=complete --timeout=300 job/${CRONJOB}-manual &
 completion_pid=$!
 
 # wait for failure as background process - capture PID
-kubectl -n magento wait --for=condition=failed job/ap3-stg-magento-setup-upgrade-manual && exit 1 &
+kubectl -n $NAMESPACE wait --for=condition=failed --timeout=300 job/${CRONJOB}-manual && exit 1 &
 failure_pid=$!
 
 # capture exit code of the first subprocess to exit
