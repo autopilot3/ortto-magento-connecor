d2d0c8798688bcf43053ec7f31991eb26c3bdb39
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-02-02 15:46:18 +1100

    deployment script

---
+++ .buildkite/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "--- Getting Magento pod name"
+POD=$(kubectl get pods -l autopilothq.com/name=magento --namespace magento -o jsonpath='{.items[*].metadata.name}' --field-selector status.phase=Running --sort-by=.metadata.creationTimestamp | head -n 1)
+echo "Pod name is $POD"
+
+echo "--- Packaging"
+tar cfvz /tmp/magento-connector-php.tgz --exclude=.git --exclude=.gitignore --exclude=.buildkite .
+
+echo "--- Copying to $POD"
+kubectl cp -n magento /tmp/magento-connector-php.tgz "$POD":/tmp/
+
+echo "--- Trigger php bin/magento setup:upgrade"
+
+echo "Deleting existing job if it exists"
+kubectl -n magento delete job magento-setup-upgrade-manual
+
+echo "Triggering new job from cronjob"
+kubectl -n magento create job --from=cronjob/magento-setup-upgrade magento-setup-upgrade-manual
