646ff19adefe124fe24adf2d617f832ac150fa1d
Alex Golshani <xitonix.io@gmail.com>
2022-06-14 21:30:27 +1000

    More logs

--- Controller/Cart/Get.php
+++ Controller/Cart/Get.php
@@ -8,7 +8,7 @@ use Ortto\Connector\Controller\AbstractJsonController;
 use Ortto\Connector\Helper\Config;
 use Ortto\Connector\Logger\OrttoLoggerInterface;
 use Ortto\Connector\Model\Api\CartDataFactory;
-use Magento\Checkout\Model\Cart;
+use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\Result\Json;
 use Magento\Backend\App\Action\Context;
@@ -18,23 +18,21 @@ class Get extends AbstractJsonController implements HttpGetActionInterface
     private CartDataFactory $cartDataFactory;
     private TrackDataProviderInterface $trackDataProvider;
     private OrttoLoggerInterface $logger;
-    private Cart $cart;
+    private Session $session;
 
     public function __construct(
         Context $context,
         OrttoLoggerInterface $logger,
         CartDataFactory $cartDataFactory,
         TrackDataProviderInterface $trackDataProvider,
-        // NOTE: Magento\Checkout\Model\Session's getQuote returns null when caching is enabled
-        // https://github.com/magento/magento2/issues/6624
-        // Using `Cart` until we find a workaround
-        Cart $cart
+        Session $session
     ) {
         parent::__construct($context, $logger);
         $this->cartDataFactory = $cartDataFactory;
         $this->trackDataProvider = $trackDataProvider;
         $this->logger = $logger;
-        $this->cart = $cart;
+        $this->session = $session;
+        $logger->info("CONST", ['has' => $session->hasQuote(), 'is_ajax' => $this->getRequest()->isAjax()]);
     }
 
     /**
@@ -53,9 +51,20 @@ class Get extends AbstractJsonController implements HttpGetActionInterface
             if (empty($productIds)) {
                 return $this->error("No product was added to the cart");
             }
+            $this->logger->info(
+                "EXEC",
+                [
+                    'has' => $this->session->hasQuote(),
+                    'is_ajax' => $this->getRequest()->isAjax(),
+                    'quote_id' => $this->session->getQuoteId(),
+                    'name' => $this->session->getName(),
+                    'last_real_q_id' => $this->session->getLastRealOrder()->getQuoteId(),
+                ]
+            );
             $trackingData = $this->trackDataProvider->getData();
             $cartData = $this->cartDataFactory->create();
-            if ($cartData->load($this->cart->getQuote())) {
+
+            if ($cartData->load($this->session->getQuote())) {
                 $payload = [
                     'event' => Config::EVENT_TYPE_PRODUCT_ADDED_TO_CART,
                     'scope' => $trackingData->getScope()->toArray(),
